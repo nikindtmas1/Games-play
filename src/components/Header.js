8a8fa4ca0464bf6247b85c0ff3519779015eb825
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-11-22 11:43:34 +0200

    Added guest and user logic

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,34 @@
 import { Link } from 'react-router-dom';
 
 const Header = ({
-
+  isAuthenticated,
+  username
 }) => {
 
+  let guestNavigation = (
+    <div id="guest">
+    <Link to="/login">Login</Link>
+    <Link to="/register">Register</Link>
+  </div>
+  );
 
+  let userNavigation = (
+    <div id="user">
+    <Link to="/create-game">Create Game</Link>
+    <Link to="/logout">Logout</Link>
+  </div>
+  )
 
   return (
       <header>
       <h1><Link class="home" to="/home">GamesPlay</Link></h1>
       <nav>
         <Link to="/games">All games</Link>
-        <div id="user">
-          <Link to="/create-game">Create Game</Link>
-          <Link to="/logout">Logout</Link>
-        </div>
-        <div id="guest">
-          <Link to="/login">Login</Link>
-          <Link to="/register">Register</Link>
-        </div>
+        
+       {isAuthenticated
+        ? userNavigation
+        : guestNavigation
+       }
       </nav>
       </header>
   );
